14a88002add4b21af863b190a4871012eea6e767
Toye <toyeseaderibigbe@gmail.com>
2020-05-03 10:10:59 +0100

    update 1.0

--- intro-component-with-signup-form-master/script.js
+++ intro-component-with-signup-form-master/script.js
@@ -1,13 +1,50 @@
-const first= document.getElementById('firstname');
-const last= document.getElementById('lastname');
-const email= document.getElementById('email');
-const password= document.getElementById('password');
+
 const form = document.getElementById('form');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    const firstValue = form['firstname'].value;
-    alert(firstValue);
+    const first = form['firstname'].value;
+    const last= form['lastname'].value;
+    const email= form['email'].value;
+    const password=form['password'].value;
+    
+    if (first===''){
+        addErrorTo('firstname', 'First Name is required');  
+    }else{
+        removeError('firstname');
+    }
+    if (last===''){
+        addErrorTo('lastname', 'Last Name is required');  
+    }else{
+        removeError('lastname');
+    }
+    if (email ===''){
+        addErrorTo('email', 'Email is required');  
+    }else if (!isValid[email]){
+        addErrorTo('email', 'Email is not valid');  
+    }else{
+        removeError('email');
+    }
+    if (password===''){
+        addErrorTo('password', 'Password is required');  
+    }else{
+        removeError('password');
+    }
 
 });
+function addErrorTo(field,message){
+    const formControl = form[field].parentNode;
+    formControl.classList.add('error');
+
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+function removeError(field) {
+    const formControl = form[field].parentNode;
+    formControl.classList.remove('error');
+}
+function isValid(email) {
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
